dec2fd7fdcb02970b07e8325f7e3a442851c6fa1
liveasx <liveasx@live.com>
2020-06-12 06:22:07 +0800

    deploy

---
+++ assets/js/bd-tj.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?4fafe153440af7a2ff5e19ffb9cea245";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
---
+++ assets/js/google-ad.js
@@ -0,0 +1,5 @@
+google_ad_client = "ca-pub-7376980002071071";
+/* 728X90广告 */
+google_ad_slot = "6097511339";
+google_ad_width = 728;
+google_ad_height = 90;
